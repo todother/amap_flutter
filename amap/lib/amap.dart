065569d76b31eeb503475c6b4499f9a2e3ce4627
geyan <geyan@geyandeMacBook-Pro.local>
2020-02-23 20:45:40 +0800

    test

--- amap/lib/amap.dart
+++ amap/lib/amap.dart
@@ -3,11 +3,19 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Amap {
-  static const MethodChannel _channel =
-      const MethodChannel('amap');
+  static const MethodChannel _channel = const MethodChannel('amap');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> initAmap() async {
+    var result = await _channel.invokeMethod("init");
+    if (result["result"] == "OK") {
+      return "OK";
+    } else {
+      return "Failed";
+    }
   }
 }
